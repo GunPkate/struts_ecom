5365bdbf95c41f2124197e509d762743aa029784
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-05-05 19:36:23 +0700

    import struct2 and action support dependencies

--- src/main/java/action/LoginAction.java
+++ src/main/java/action/LoginAction.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import org.apache.struts2.dispatcher.Parameter.Request;
-
 import com.opensymphony.xwork2.ActionSupport;
 import model.User;
 
@@ -54,31 +52,29 @@ public class LoginAction extends ActionSupport {
       String  userDB = "root";
       String  passwordDB = "root";
 
-      try {
-
-      Class.forName("org.postgresql.Driver");
-      conn = DriverManager.getConnection(URL, userDB, passwordDB);
-      String sql = "SELECT * FROM users WHERE user = ? AND password = ?";
-      PreparedStatement ps = conn.prepareStatement(sql);
-      ps.setString(1, userData);
-      ps.setString(2, passwordData);
-      ResultSet rs = ps.executeQuery();
-
-      while (rs.next()) {
-            name = rs.getString(1);
-            System.out.println(name);
-            ret = SUCCESS;
-      }
-      } catch (Exception e) {
-         System.out.print("Error");
-      } finally {
-         if (conn != null) {
-            try {
-               conn.close();
-            } catch (Exception e) {
-            }
-         }
-      }
+//      try {
+//
+//      String sql = "SELECT * FROM users WHERE user = ? AND password = ?";
+//      PreparedStatement ps = conn.prepareStatement(sql);
+//      ps.setString(1, userData);
+//      ps.setString(2, passwordData);
+//      ResultSet rs = ps.executeQuery();
+//
+//      while (rs.next()) {
+//            name = rs.getString(1);
+//            System.out.println(name);
+//            ret = SUCCESS;
+//      }
+//      } catch (Exception e) {
+//         System.out.print("Error");
+//      } finally {
+//         if (conn != null) {
+//            try {
+//               conn.close();
+//            } catch (Exception e) {
+//            }
+//         }
+//      }
       return ret;
    }
 
